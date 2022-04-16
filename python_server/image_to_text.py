b4b2fee8db04b3a90b63dac9c892bc46fcda09ad
Sankalp Mane <sankalp.m.1929@gmail.com>
2022-04-16 15:37:49 +0530

    uvicorn command changed

--- python_server/image_to_text.py
+++ python_server/image_to_text.py
@@ -233,6 +233,3 @@ async def main():
     """
     return HTMLResponse(content=content)
     
-
-if __name__=="__main__":
-    uvicorn.run("image_to_text:app",host='0.0.0.0', port=8000, reload=True)
